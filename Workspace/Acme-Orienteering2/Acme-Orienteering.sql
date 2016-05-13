68747a7a222a2c9511c7ba608d8bd51a5010d80e
migrodcab <miguel.rc95@gmail.com>
2016-05-13 02:19:34 +0200

    Borrando duplicado

--- Workspace/Acme-Orienteering2/Acme-Orienteering.sql
+++
@@ -1,6 +0,0 @@
-drop database if exists `Acme-Orienteering`;
-create database `Acme-Orienteering`;
-grant select, insert, update, delete
-on `Acme-Orienteering`.* to 'acme-user'@'%';
-grant select, insert, update, delete, create, drop, references, index, alter, create temporary tables, lock tables, create view, create routine, alter routine, execute, trigger, show view
-on `Acme-Orienteering`.* to 'acme-manager'@'%';
