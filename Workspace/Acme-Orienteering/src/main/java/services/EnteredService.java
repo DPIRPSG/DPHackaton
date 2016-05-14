f2c27e23658833c84c75a9303a0a7c45bdd0ef6c
guillermo-ag-95 <guillermo.ag.95@gmail.com>
2016-05-14 18:32:53 +0200

    Añadiendo asserts y comentando métodos

--- Workspace/Acme-Orienteering/src/main/java/services/EnteredService.java
+++ Workspace/Acme-Orienteering/src/main/java/services/EnteredService.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import domain.Entered;
 
@@ -20,6 +21,8 @@ public class EnteredService {
 	private EnteredRepository enteredRepository;
 	
 	// Supporting services ----------------------------------------------------
+	@Autowired
+	private ActorService actorService;
 
 	// Constructors -----------------------------------------------------------
 	
@@ -29,7 +32,17 @@ public class EnteredService {
 	
 	// Simple CRUD methods ----------------------------------------------------
 	
+	/**
+	 * 
+	 * @param runnerId
+	 * @see 21.b
+	 * 	Un usuario que haya iniciado sesión como corredor debe poder:
+	 * 	Revisar el estado de las distintas peticiones que haya realizado.
+	 * @return the collection of entered done by a runner
+	 */
 	public Collection<Entered> findAllByRunner(int runnerId){
+		Assert.isTrue(actorService.checkAuthority("RUNNER"));
+		
 		Collection<Entered> result;
 		
 		result = enteredRepository.findAllByRunner(runnerId);
@@ -37,7 +50,17 @@ public class EnteredService {
 		return result;
 	}
 	
+	/**
+	 * 
+	 * @param clubId
+	 * @see 22.c
+	 * 	Un usuario que haya iniciado sesión como gerente debe poder:
+	 * 	Revisar las distintas peticiones de ingreso que le han hecho al club.
+	 * @return the collection of entered that a club receive.
+	 */
 	public Collection<Entered> findAllByClub(int clubId){
+		Assert.isTrue(actorService.checkAuthority("MANAGER"));
+		
 		Collection<Entered> result;
 		
 		result = enteredRepository.findAllByClub(clubId);
@@ -45,7 +68,17 @@ public class EnteredService {
 		return result;
 	}
 	
+	/**
+	 * 
+	 * @param clubId
+	 * @see
+	 * 	Un usuario que haya iniciado sesión como gerente debe poder:
+	 * 	Aceptar o denegar las peticiones.
+	 * @return the collection of entered not accepted yet that a club have received.
+	 */
 	public Collection<Entered> findAllToReviseByClub(int clubId){
+		Assert.isTrue(actorService.checkAuthority("MANAGER"));
+		
 		Collection<Entered> result;
 		
 		result = enteredRepository.findAllToReviseByClub(clubId);
