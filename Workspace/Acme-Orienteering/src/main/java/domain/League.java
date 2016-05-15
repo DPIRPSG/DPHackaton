076e3359d57c7db0270dbb19e0dd6c5b8210524c
Carlos Alberto Mata Gil <CeuxDruman@gmail.com>
2016-05-15 17:48:56 +0200

    Populate todo con 1, a falta de message

--- Workspace/Acme-Orienteering/src/main/java/domain/League.java
+++ Workspace/Acme-Orienteering/src/main/java/domain/League.java
@@ -17,7 +17,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.URL;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
