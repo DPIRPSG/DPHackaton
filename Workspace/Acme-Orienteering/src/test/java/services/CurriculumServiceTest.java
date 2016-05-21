4e34de998e0fdc9e0087bb58f31b82b0da350ea2
migrodcab <miguel.rc95@gmail.com>
2016-05-21 17:31:20 +0200

    TestEditCurriculum

--- Workspace/Acme-Orienteering/src/test/java/services/CurriculumServiceTest.java
+++ Workspace/Acme-Orienteering/src/test/java/services/CurriculumServiceTest.java
@@ -339,5 +339,270 @@ public class CurriculumServiceTest extends AbstractTest {
 		//authenticate(null);
 	}
 	
+	/**
+	 * Acme-Orienteering - 
+	 */
 	
+	/**
+	 * Test que comprueba que un usuario del sistema
+	 * puede editar su curriculum en condiciones normales
+	 */
+	@Test
+	public void testEditCurriculumOk() {
+		Curriculum curriculum;
+		Collection<Actor> actors;
+		Actor actor;
+		
+		actors = actorService.findAll();
+		actor = null;
+		
+		for(Actor a : actors) {
+			if(a.getCurriculum() != null) {
+				actor = a;
+				break;
+			}
+		}
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(actor.getUserAccount().getUsername());
+		
+		curriculum = actor.getCurriculum();
+		curriculum.setStatement("Prueba");
+		curriculum.setSkills("Prueba");
+		curriculum.setLikes("Prueba");
+		curriculum.setDislikes("Prueba");
+		curriculum = curriculumService.save(curriculum);
+		
+		actor = actorService.findByPrincipal();
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		Assert.isTrue(actor.getCurriculum().getStatement().equals("Prueba"));
+		Assert.isTrue(actor.getCurriculum().getSkills().equals("Prueba"));
+		Assert.isTrue(actor.getCurriculum().getLikes().equals("Prueba"));
+		Assert.isTrue(actor.getCurriculum().getDislikes().equals("Prueba"));
+		
+		authenticate(null);
+	}
+	
+	
+	/**
+	 * Acme-Orienteering - 
+	 */
+	
+	/**
+	 * Test que comprueba que un usuario del sistema no
+	 * puede editar su curriculum sin statement
+	 */
+	@Test(expected=ConstraintViolationException.class)
+	@Rollback(value=true)
+	public void testEditCurriculumError1() {
+		Curriculum curriculum;
+		Collection<Actor> actors;
+		Actor actor;
+		
+		actors = actorService.findAll();
+		actor = null;
+		
+		for(Actor a : actors) {
+			if(a.getCurriculum() != null) {
+				actor = a;
+				break;
+			}
+		}
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(actor.getUserAccount().getUsername());
+		
+		curriculum = actor.getCurriculum();
+		curriculum.setStatement(null);
+		curriculum.setSkills("Prueba");
+		curriculum.setLikes("Prueba");
+		curriculum.setDislikes("Prueba");
+		curriculum = curriculumService.save(curriculum);
+		curriculumService.flush();
+		
+		actor = actorService.findByPrincipal();
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(null);
+	}
+	
+	/**
+	 * Acme-Orienteering - 
+	 */
+	
+	/**
+	 * Test que comprueba que un usuario del sistema no
+	 * puede editar su curriculum sin skills
+	 */
+	@Test(expected=ConstraintViolationException.class)
+	@Rollback(value=true)
+	public void testEditCurriculumError2() {
+		Curriculum curriculum;
+		Collection<Actor> actors;
+		Actor actor;
+		
+		actors = actorService.findAll();
+		actor = null;
+		
+		for(Actor a : actors) {
+			if(a.getCurriculum() != null) {
+				actor = a;
+				break;
+			}
+		}
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(actor.getUserAccount().getUsername());
+		
+		curriculum = actor.getCurriculum();
+		curriculum.setStatement("Prueba");
+		curriculum.setSkills(null);
+		curriculum.setLikes("Prueba");
+		curriculum.setDislikes("Prueba");
+		curriculum = curriculumService.save(curriculum);
+		curriculumService.flush();
+		
+		actor = actorService.findByPrincipal();
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(null);
+	}
+	
+	/**
+	 * Acme-Orienteering - 
+	 */
+	
+	/**
+	 * Test que comprueba que un usuario del sistema no
+	 * puede editar su curriculum sin likes
+	 */
+	@Test(expected=ConstraintViolationException.class)
+	@Rollback(value=true)
+	public void testEditCurriculumError3() {
+		Curriculum curriculum;
+		Collection<Actor> actors;
+		Actor actor;
+		
+		actors = actorService.findAll();
+		actor = null;
+		
+		for(Actor a : actors) {
+			if(a.getCurriculum() != null) {
+				actor = a;
+				break;
+			}
+		}
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(actor.getUserAccount().getUsername());
+		
+		curriculum = actor.getCurriculum();
+		curriculum.setStatement("Prueba");
+		curriculum.setSkills("Prueba");
+		curriculum.setLikes(null);
+		curriculum.setDislikes("Prueba");
+		curriculum = curriculumService.save(curriculum);
+		curriculumService.flush();
+		
+		actor = actorService.findByPrincipal();
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(null);
+	}
+	
+	/**
+	 * Acme-Orienteering - 
+	 */
+	
+	/**
+	 * Test que comprueba que un usuario del sistema no
+	 * puede editar su curriculum sin Dislikes
+	 */
+	@Test(expected=ConstraintViolationException.class)
+	@Rollback(value=true)
+	public void testEditCurriculumError4() {
+		Curriculum curriculum;
+		Collection<Actor> actors;
+		Actor actor;
+		
+		actors = actorService.findAll();
+		actor = null;
+		
+		for(Actor a : actors) {
+			if(a.getCurriculum() != null) {
+				actor = a;
+				break;
+			}
+		}
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(actor.getUserAccount().getUsername());
+		
+		curriculum = actor.getCurriculum();
+		curriculum.setStatement("Prueba");
+		curriculum.setSkills("Prueba");
+		curriculum.setLikes("Prueba");
+		curriculum.setDislikes(null);
+		curriculum = curriculumService.save(curriculum);
+		curriculumService.flush();
+		
+		actor = actorService.findByPrincipal();
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		authenticate(null);
+	}
+	
+	/**
+	 * Acme-Orienteering - 
+	 */
+	
+	/**
+	 * Test que comprueba que un usuario del sistema no
+	 * puede editar un curriculum sin estar logueado
+	 */
+	@Test(expected=IllegalArgumentException.class)
+	@Rollback(value=true)
+	public void testEditCurriculumError5() {
+		Curriculum curriculum;
+		Collection<Actor> actors;
+		Actor actor;
+		
+		actors = actorService.findAll();
+		actor = null;
+		
+		for(Actor a : actors) {
+			if(a.getCurriculum() != null) {
+				actor = a;
+				break;
+			}
+		}
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		//authenticate(actor.getUserAccount().getUsername());
+		
+		curriculum = actor.getCurriculum();
+		curriculum.setStatement("Prueba");
+		curriculum.setSkills("Prueba");
+		curriculum.setLikes("Prueba");
+		curriculum.setDislikes("Prueba");
+		curriculum = curriculumService.save(curriculum);
+		curriculumService.flush();
+		
+		actor = actorService.findByPrincipal();
+		
+		Assert.isTrue(actor.getCurriculum() != null);
+		
+		//authenticate(null);
+	}
 }
